22039b0b20313fa0cb8e92d740ecdb9d5ffb3732
Castro Agbo <ceafive@gmail.com>
2022-11-03 19:29:46 +0000

    update App.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,14 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useAppDispatch } from "./app/hooks";
 import Header from "./components/Header";
 import SensorsDisplay from "./components/SensorsDisplay";
-import { SensorInterface } from "./features/interfaces";
-import {
-  fetchSensorsAsync,
-  setSensors,
-  socket,
-} from "./features/sensors/sensorsSlice";
+import { fetchSensorsAsync } from "./features/sensors/sensorsSlice";
 
 function App() {
-  const wsRef = useRef<WebSocket>();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    // dispatch(fetchSensorsAsync());
-
-    const ws = socket;
-    ws.onopen = () => {
-      console.log("onopen");
-    };
-    ws.onclose = () => console.log("onclose");
-    ws.onmessage = (e) => {
-      const data = JSON.parse(e.data) as SensorInterface;
-      dispatch(setSensors(data));
-    };
-    wsRef.current = ws;
+    dispatch(fetchSensorsAsync());
   }, [dispatch]);
 
   return (
